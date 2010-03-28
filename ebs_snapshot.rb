ae3e3e6f3ebc627ac4d591615f256b46bb1f04db
Andrew Bruce <andrew@camelpunch.com>
2010-03-28 19:16:21 +0100

    EBS snapshot script

---
+++ ebs_snapshot.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'right_aws'
+require 'active_support'
+
+unless ENV['SNAPSHOT_VOLUME']
+  raise "Please provide a volume ID with SNAPSHOT_VOLUME environment variable"
+end
+
+ec2 = Rightscale::Ec2.new(ENV['AMAZON_ACCESS_KEY_ID'], 
+                          ENV['AMAZON_SECRET_ACCESS_KEY'], 
+                          :region => (ENV['REGION'] || 'eu-west-1'))
+
+snapshots = ec2.describe_snapshots
+
+old_snapshots = snapshots.select do |snapshot|
+  (snapshot[:aws_started_at] < 1.week.ago) &&
+    snapshot[:aws_volume_id] == ENV['SNAPSHOT_VOLUME']
+end
+
+snapshots_since_this_morning = snapshots.select do |snapshot|
+  (snapshot[:aws_started_at] > Date.today) &&
+    snapshot[:aws_volume_id] == ENV['SNAPSHOT_VOLUME']
+end
+
+old_snapshots.each do |snapshot|
+  puts "Deleting snapshot #{snapshot[:aws_id]} from #{snapshot[:aws_started_at]}"
+  ec2.delete_snapshot snapshot[:aws_id]
+end
+
+if snapshots_since_this_morning.empty?
+  puts "Creating snapshot"
+  puts ec2.create_snapshot(ENV['SNAPSHOT_VOLUME']).to_yaml
+else
+  puts "The following snapshots were created since this morning:"
+  puts snapshots_since_this_morning.to_yaml
+end
+
