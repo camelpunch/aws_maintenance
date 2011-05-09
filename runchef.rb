b9b62952dc8bbfe554dab9c0030c702ff51ac4a6
Andrew Bruce <andrew@camelpunch.com>
2011-05-09 22:07:58 +0100

    use OptionParser in runchef.rb

--- runchef.rb
+++ runchef.rb
@@ -3,23 +3,49 @@
 require 'rubygems'
 require 'right_aws'
 require 'ruby-debug'
+require 'optparse'
 
-site = ARGV[0]
-sdf_volume = ARGV[1]
+options = {
+  :ami => 'ami-359ea941',
+  :group => 'default',
+  :size => 'm1.small',
+}
 
-group = ARGV[2] || 'default'
-ami = ARGV[3] || 'ami-8d3502f9'
-size = ARGV[4] || 'm1.small'
+cmdline_options = OptionParser.new do |opts|
+  opts.banner = "Usage: ruchef.rb [options]"
+
+  opts.on '-r', '--role ROLE' do |role|
+    options[:role] = role
+  end
+
+  opts.on '-v', '--sdf-volume VOLUME' do |sdf|
+    options[:sdf_volume] = sdf
+  end
+
+  opts.on '-g', '--group GROUP' do |group|
+    options[:group] = group
+  end
+
+  opts.on '-a', '--ami AMI' do |ami|
+    options[:ami] = ami
+  end
+
+  opts.on '-s', '--size SIZE' do |size|
+    options[:size] = size
+  end
+end
+
+cmdline_options.parse!
 
 ec2 = RightAws::Ec2.new(ENV['AMAZON_ACCESS_KEY_ID'], ENV['AMAZON_SECRET_ACCESS_KEY'])
-instances = ec2.run_instances(ami,
+instances = ec2.run_instances(options[:ami],
                               min = 1,
                               max = 1,
-                              groups = [group],
+                              groups = [options[:group]],
                               key = 'camelpunch',
-                              user_data = File.read("/Users/andrew/dev/chef-repo/roles/#{site}-data.json"),
-                              "public",
-                              size,
+                              user_data = File.read("/Users/andrew/dev/chef-repo/roles/#{options[:role]}-data.json"),
+                              addressing = "public",
+                              options[:size],
                               kernel = nil,
                               ramdisk = nil,
                               zone = "eu-west-1a",
@@ -43,9 +69,9 @@ while state != 'running'
   sleep 2
 end
 
-if sdf_volumn && !sdf_volume.empty?
-  puts "attaching #{sdf_volume} to /dev/sdf"
-  ec2.attach_volume(sdf_volume, instance_id, '/dev/sdf')
+if options[:sdf_volume]
+  puts "attaching #{options[:sdf_volume]} to /dev/sdf"
+  ec2.attach_volume(options[:sdf_volume], instance_id, '/dev/sdf')
 end
 
 dns_name = ''
